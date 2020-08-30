bda0ddbdcf0fc01efe2f916c1e3f9fe3c950bea7
Andreas Blaesius <andi@unlegacy-android.org>
2020-08-30 20:16:08 +0200

    Not working yet... still

--- dist/photoswipe.js
+++ dist/photoswipe.js
@@ -3279,10 +3279,6 @@ _registerModule('DesktopZoom', {
 				return;
 			}
 
-			self.setupDesktopZoom();
-
-			/* There seems to be a timing problem so this may run before a mouseevent is detected in which case
-			   setupDesktopZoom is not run if _likelyTouchDevice was set to true. Just run it all the time.
 			if(_likelyTouchDevice) {
 				// if detected hardware touch support, we wait until mouse is used,
 				// and only then apply desktop-zoom features
@@ -3292,7 +3288,6 @@ _registerModule('DesktopZoom', {
 			} else {
 				self.setupDesktopZoom(true);
 			}
-			*/
 		},
 
 		setupDesktopZoom: function (onInit) {
